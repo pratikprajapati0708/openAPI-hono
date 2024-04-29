03595bad1450af862374bb97631dc763dea94622
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-04-29 20:42:36 +0530

    created swagger ui in hono

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { createRoute,OpenAPIHono } from '@hono/zod-openapi'
 import { ParamsSchema } from './inputs'
 import { UserSchema } from './outputs'
 import { Hono } from 'hono'
+import { swaggerUI } from '@hono/swagger-ui'
 
 const app = new OpenAPIHono()
 const userroute = createRoute({
@@ -38,7 +39,7 @@ app.doc('/doc', {
     title: 'My API',
   },
 })
-
+app.get('/ui', swaggerUI({ url: '/doc' }))
 
 export default app
 
